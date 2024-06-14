15f9292fd53c1932fc4e0d1cb3432da570032c3b
shucksow <102425362+shucksow@users.noreply.github.com>
2024-06-14 14:30:12 -0400

    feat: Add grid builder functionality with slider

---
+++ script.js
@@ -0,0 +1,38 @@
+let normalBtn = document.querySelector("#normalBtn");
+let rainbowBtn = document.querySelector("#rainbowBtn");
+let eraserBtn = document.querySelector("#eraserBtn");
+let clearBtn = document.querySelector("#clearBtn");
+let sliderVal = document.querySelector("#slider-value");
+let slider = document.querySelector("#myRange");
+let parentContainer = document.querySelector("#canvas");
+
+function buildCanvas() {
+    for (let i = 0; i < slider.value; i++) {
+        const newRow = document.createElement("div");
+        newRow.setAttribute("class", "row");
+        parentContainer.appendChild(newRow);
+
+        for (let j = 0; j < slider.value; j++) {
+            const  newColumn = document.createElement("div");
+            newColumn.setAttribute("class", "col");
+            newRow.appendChild(newColumn);
+
+        }
+    }
+}
+
+function deleteGrid() {
+    while (parentContainer.hasChildNodes()) {
+        parentContainer.removeChild(parentContainer.firstChild);
+    }
+}
+
+
+buildCanvas();
+
+slider.addEventListener('input', function() {
+    deleteGrid();
+    sliderVal.textContent = slider.value + "x" + slider.value;
+    buildCanvas();
+});
+
